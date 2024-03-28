8a67b66b033bfaa247f3f63b8cbcd833d311c021
brunomry <brunomadozzo@gmail.com>
2024-03-28 14:52:24 -0300

    En index.js realice importacion y configuracion de midlewares ademas de importar express y configurar puerto

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import morgan from "morgan";
+import {fileURLToPath} from "url";
+import path from "path";
+
+const app = express(); 
+app.set("port", process.env.PORT || 4000);
+app.listen(app.get("port"), ()=>{
+console.log("Estoy en el puerto "+app.get("port"));
+});
+
+app.use(cors())
+app.use(morgan())
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "/public")))

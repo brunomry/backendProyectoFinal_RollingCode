b4b4b2abc82811d947d43ac37d9e109e4f16dfb2
Elias Juarez <juarezelias83@gmail.com>
2024-04-04 20:42:06 -0300

    se agregaron los requerimientos de la categoria del producto en el Schema

--- src/database/model/producto.js
+++ src/database/model/producto.js
@@ -1,21 +1,32 @@
 import mongoose, { Schema } from "mongoose";
 
 const productoSchema = new Schema({
-    nombre:{
-        type: String,
-        required: true,
-        unique: true,
-        minLength: 5,
-        maxLength: 25,
-    },
-    precio:{
-        type: Number,
-        required: true,
-        min: 3500,
-        max: 10000,
-    },
-    imagen:{
-        type: String,
-        required: true,
-    }
-})
+  nombre: {
+    type: String,
+    required: true,
+    unique: true,
+    minLength: 5,
+    maxLength: 25
+  },
+  precio: {
+    type: Number,
+    required: true,
+    min: 3500,
+    max: 10000
+  },
+  imagen: {
+    type: String,
+    required: true
+  },
+  descripcion: {
+    type: String,
+    required: true,
+    minLength: 10,
+    maxLength: 50
+  },
+  categoria:{
+    type: String,
+    required: true,
+    enum: ["pizzas","hamburguesas","empanadas","pastas"]
+  },
+});

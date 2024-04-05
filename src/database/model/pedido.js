07c7d357bdd29c2d37a4dbe8fab27b0be253c2bc
Leonel <leonelrcordero22@gmail.com>
2024-04-04 22:09:21 -0300

    Se cambio de ubicacion el schema de pedido, era una ubicacion erronea, y se agrego el un array para almacenar los productos de los pedidos.)

---
+++ src/database/model/pedido.js
@@ -0,0 +1,28 @@
+import mongoose, { Schema } from 'mongoose';
+
+const pedidoSchema = new Schema({
+  usuario: {
+    type: String,
+    required: true,
+    minLength: 8,
+    maxLength: 50,
+  },
+  monto: {
+    type: Number,
+    required: true,
+  },
+  productos: [{ type: Schema.Types.ObjectId, ref: 'producto' }],
+  metodoEnvio: {
+    type: Number,
+    required: true,
+    enum: [1, 2],
+  },
+  estadoEnvio: {
+    type: Boolean,
+    required: true,
+  },
+});
+
+const Pedido = mongoose.model('pedido', pedidoSchema);
+
+export default Pedido;

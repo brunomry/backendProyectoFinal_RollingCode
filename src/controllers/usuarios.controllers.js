1ea15ef30606fa976daa5c5bbbc80ae105274e14
brunomry <brunomadozzo@gmail.com>
2024-04-08 01:50:08 -0300

    Agregue funcion login en controlador de usuarios y su ruta con solicitud post

--- src/controllers/usuarios.controllers.js
+++ src/controllers/usuarios.controllers.js
@@ -37,3 +37,36 @@ export const crearUsuario = async (req, res) => {
     });
   }
 };
+
+export const login = async (req,res) => {
+  try {
+    const { correo, clave} = req.body;
+
+    const usuarioBuscado = await Usuario.findOne({correo});
+
+    if(!usuarioBuscado){
+      return res.status(400).json({
+        mensaje: "El correo es incorrecto",
+      });
+    }
+
+    const claveValida = await Usuario.findOne({clave});
+
+    if (!claveValida) {
+      return res.status(400).json({
+        mensaje: "La contraseña es incorrecta",
+      });
+    }
+
+    res.status(200).json({
+      mensaje: "Los datos del usuario son correctos",
+      correo: correo
+    });
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      mensaje: "Error al intentar iniciar sesión un usuario.",
+    });
+  }
+}

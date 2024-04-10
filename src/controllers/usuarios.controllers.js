692f14876a7478f3e4e9d5e32620aa51e994cf17
Fernando Herrera <herrera770@outlook.es>
2024-04-10 10:30:09 -0300

    se agregaron las validaciones necesarias de expressvalidator a la ruta /login en la funcion post (validacionLogin)

--- src/controllers/usuarios.controllers.js
+++ src/controllers/usuarios.controllers.js
@@ -57,7 +57,7 @@ export const login = async (req, res) => {
   } catch (error) {
     console.error(error);
     res.status(500).json({
-      mensaje: "Error al intentar iniciar sesión un usuario.",
+      mensaje: "Error al intentar iniciar sesión",
     });
   }
 };
--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import { crearUsuario, login } from '../controllers/usuarios.controllers.js';
 import validacionRegistro from '../helpers/validacionesDeRegistro.js';
+import validacionLogin from '../helpers/validacionDeLogin.js';
 
 
 const enrutador = Router();
 
 enrutador.route('/registro').post([validacionRegistro],crearUsuario);
-enrutador.route('/login').post(login);
+enrutador.route('/login').post([validacionLogin],login);
 
 export default enrutador;

705f35410366b053b81d1e3d6dedc48d0808fedd
Fernando Herrera <herrera770@outlook.es>
2024-04-11 23:03:04 -0300

    se agregaron las validaciones de productos en validacionDeProductos y son invocadas en productos.routes

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -6,10 +6,12 @@ import {
   borrarProducto,
   editarProducto,
 } from '../controllers/productos.controllers.js';
+import validacionProducto from '../helpers/validacionDeProductos.js';
+
 
 const enrutador = Router();
 
-enrutador.route('/productos').get(listarProductos).post(crearProducto);
-enrutador.route('/producto/:id').get(obtenerProducto).delete(borrarProducto).put(editarProducto)
+enrutador.route('/productos').get(listarProductos).post([validacionProducto],crearProducto);
+enrutador.route('/producto/:id').get(obtenerProducto).delete(borrarProducto);
 
 export default enrutador;

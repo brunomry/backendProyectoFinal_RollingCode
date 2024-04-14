48a106cb2ef5969b4365b076b64788935ece7213
Fernando Herrera <herrera770@outlook.es>
2024-04-14 13:47:57 -0300

    se agregaron las dependencias nodemailer, fs-extra y handlebars. Se creó el helper de configuracion mailer.js

---
+++ src/helpers/mailer.js
@@ -0,0 +1,13 @@
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+});
+
+export default transporter

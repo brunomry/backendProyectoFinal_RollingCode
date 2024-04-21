c2d76daf49899962dad46b83e4a1b5b4157be97f
Fernando Herrera <herrera770@outlook.es>
2024-04-20 21:09:46 -0300

    se eliminó la validacion de JWT para la funcion crearUsuarioAdmin

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -15,6 +15,6 @@ const enrutador = Router();
 enrutador.route('/registro').post([validacionRegistro], crearUsuario);
 enrutador.route('/login').post([validacionLogin], login).get(obtenerUsuarios);
 enrutador.route('/usuario/:id').post([validarJWT],editarUsuario);
-enrutador.route('/crearusuario').post([validarJWT, validacionCrearUsuarioAdmin],crearUsuarioAdmin)
+enrutador.route('/crearusuario').post([validacionCrearUsuarioAdmin],crearUsuarioAdmin)
 
 export default enrutador;

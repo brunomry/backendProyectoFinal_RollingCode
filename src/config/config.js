ebeacbc73eddfb9d93bafc56e03621b74c7dee7f
Bruno Madozzo <brunomadozzo@gmail.com>
2025-03-25 02:15:19 -0300

    correccion de variable de entorno para token

--- src/config/config.js
+++ src/config/config.js
@@ -3,7 +3,7 @@ import "dotenv/config";
 export const config = {
     port: process.env.PORT || 3000,
     dbUri: process.env.MONGODB_URI,
-    jwtSecret: process.env.JWT_SECRET,
+    jwtSecret: process.env.SECRET_JWT,
     email: process.env.EMAIL,
     email_password: process.env.EMAIL_PASSWORD,
     token_mp: process.env.TOKEN_MP,
